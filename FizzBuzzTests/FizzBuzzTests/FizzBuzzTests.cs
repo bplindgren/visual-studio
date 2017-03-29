768c9c99bc38d534532763240ed32cf29bf19978
bplindgren <bplindgren91@gmail.com>
2017-03-29 16:08:31 -0500

    tests added

---
+++ FizzBuzzTests/FizzBuzzTests/FizzBuzzTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication3;
+
+namespace FizzBuzzTests
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        FizzBuzz fb = new FizzBuzz();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(fb.sayTrue());
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsFalse(fb.sayFalse());
+        }
+    }
+}
