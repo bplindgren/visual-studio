fdd4d5f98a7a5748ccd99995361c68a1a9b7edd1
bplindgren <bplindgren91@gmail.com>
2017-03-29 19:18:07 -0500

    refactoring

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -6,65 +6,62 @@ using System.Threading.Tasks;
 
 namespace FizzBuzzApplication
 {
-    public class FizzBuzz
-    {
+    public class FizzBuzz {
         public int startNumber;
         public int endNumber;
         public string result;
-        public static string five = "buzz";
 
         // No-arg constructor
-        public FizzBuzz()
-        {
+        public FizzBuzz() {
             this.startNumber = 0;
             this.endNumber = 0;
             this.result = "";
         }
 
         // Constructor with start and end arguments
-        public FizzBuzz(int startNumber, int endNumber)
-        {
+        public FizzBuzz(int startNumber, int endNumber) {
             this.startNumber = startNumber;
             this.endNumber = endNumber;
             this.result = "";
         }
 
         // Sets the start and end numbers
-        public void SetRange(int startNumber, int endNumber)
-        {
+        public void SetRange(int startNumber, int endNumber) {
             this.startNumber = startNumber;
             this.endNumber = endNumber;
         }
 
         // Generate the output string
-        public string getOutput()
-        {
-            for (int i = this.startNumber; i <= this.endNumber; i++)
-            {
+        public string getOutput() {
+            // Loop over the range
+            for (int i = this.startNumber; i <= this.endNumber; i++) {
+                // Start with an empty output string
                 string s = "";
-                if ((i % 3) == 0)
-                {
+
+                // Build the output string accordingly
+                if ((i % 3) == 0) {
                     s += "fizz";
-                } if ((i % 5) == 0)
-                {
+                } if ((i % 5) == 0) {
                     s += "buzz";
                 }
 
-                if (String.IsNullOrWhiteSpace(s))
-                {
+                // If neither 'fizz' or 'buzz' is added, add i
+                if (String.IsNullOrWhiteSpace(s)) {
                     s += (i.ToString() + " ");
-                } else 
-                {
+                } else {
                     s += " ";
                 }
+
+                // Add the output string to the FizzBuzz object result property
                 this.result += s;
             }
+
+            // After looping over entire range, return the result
             return this.result.Substring(0, (this.result.Length - 1));
         }
 
-        static void Main(string[] args)
-        {
-            FizzBuzz fb = new FizzBuzz(1, 50);
+        static void Main(string[] args) {
+            FizzBuzz fb = new FizzBuzz(1, 20);
             Console.Write(fb.getOutput());
         }
     }
--- FizzBuzzTests/FizzBuzzTests/FizzBuzzTests.cs
+++ FizzBuzzTests/FizzBuzzTests/FizzBuzzTests.cs
@@ -5,8 +5,7 @@ using FizzBuzzApplication;
 namespace FizzBuzzTests
 {
     [TestClass]
-    public class FizzBuzzTests
-    {
+    public class FizzBuzzTests {
         // Create FizzBuzz object with default constructor
         FizzBuzz fb1 = new FizzBuzz();
 
@@ -15,43 +14,50 @@ namespace FizzBuzzTests
 
 
         [TestMethod]
-        public void fb1IsFizzBuzzObject()
-        {
+        public void fb1IsFizzBuzzObject() {
             Assert.IsInstanceOfType(fb1, typeof(FizzBuzz));
         }
 
         [TestMethod]
-        public void fb1InitialProperties()
-        {
+        public void fb1InitialProperties() {
             Assert.AreEqual(fb1.startNumber, 0);
             Assert.AreEqual(fb1.endNumber, 0);
+            Assert.AreEqual(fb1.result, "");
         }
 
         [TestMethod]
-        public void fb1SetRange()
-        {
+        public void fb1SetRange() {
             fb1.SetRange(10, 50);
             Assert.AreEqual(fb1.startNumber, 10);
             Assert.AreEqual(fb1.endNumber, 50);
         }
 
         [TestMethod]
-        public void fb2IsFizzBuzzObject()
-        {
+        public void fb2IsFizzBuzzObject() {
             Assert.IsInstanceOfType(fb2, typeof(FizzBuzz));
         }
 
         [TestMethod]
-        public void fb2InitialProperties()
-        {
+        public void fb2InitialProperties() {
             Assert.AreEqual(fb2.startNumber, 1);
             Assert.AreEqual(fb2.endNumber, 20);
+            Assert.AreEqual(fb2.result, "");
         }
 
         [TestMethod]
-        public void fb2Output()
-        {
+        public void fb1Output() {
+            Assert.AreEqual(fb1.getOutput(), "fizzbuzz");
+        }
+
+        [TestMethod]
+        public void fb2Output20() {
             Assert.AreEqual(fb2.getOutput(), "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz");
+        }
+
+        [TestMethod]
+        public void fb2Output100() {
+            fb2.SetRange(1, 100);
+            Assert.AreEqual(fb2.getOutput(), "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz fizz 22 23 fizz buzz 26 fizz 28 29 fizzbuzz 31 32 fizz 34 buzz fizz 37 38 fizz buzz 41 fizz 43 44 fizzbuzz 46 47 fizz 49 buzz fizz 52 53 fizz buzz 56 fizz 58 59 fizzbuzz 61 62 fizz 64 buzz fizz 67 68 fizz buzz 71 fizz 73 74 fizzbuzz 76 77 fizz 79 buzz fizz 82 83 fizz buzz 86 fizz 88 89 fizzbuzz 91 92 fizz 94 buzz fizz 97 98 fizz buzz");
         }
     }
 }
