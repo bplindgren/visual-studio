ffb37ad9b3d6b2846417848a6734e47c6a31d6fe
bplindgren <bplindgren91@gmail.com>
2017-04-01 15:16:09 -0500

    hangman complete

--- HangmanSolution/HangmanApplication/Hangman.cs
+++ HangmanSolution/HangmanApplication/Hangman.cs
@@ -8,16 +8,37 @@ using System.Threading.Tasks;
 namespace HangmanApplication {
     class Hangman {
         public List<string> WordList = new List<string>();
-        public List<string> Drawing = new List<string> { "__________\n", "|      | \n", "|\n", "|\n", "|\n", "|      O \n", "|      | \n", "|    --| \n", "|    --|-- \n", "|     / \n", "|     / \\ \n" };
+        public List<string> Drawing = new List<string>
+            { "\n__________\n",
+              "|      | \n",
+              "|\n",
+              "|\n",
+              "|\n",
+              "|      O \n",
+              "|      | \n",
+              "|    --| \n",
+              "|    --|-- \n",
+              "|     / \n",
+              "|     / \\ \n" };
         public List<char> IncorrectGuesses = new List<char>();
-        public char[] WordToGuess { get; set; }
+        public char[] WordToGuess;
         public char[] WordDisplay;
         public int GuessesRemaining;
 
-        public Hangman() {
-            // Populate the word list
-            foreach (string line in File.ReadLines("../../../HangManWords.txt"))
-                WordList.Add(line.ToLower());
+        public Hangman(string path) {
+            if (File.Exists(path)) {
+                // Check that the file is not empty
+                long length = new System.IO.FileInfo(path).Length;
+                if (length < 1) {
+                    throw new Exception("This file doesn't have any words in it. Sorry Charlie.");
+                } else {
+                    // Populate the word list
+                    foreach (string line in File.ReadLines(path))
+                        WordList.Add(line.ToLower());
+                }
+            } else {
+                throw new FileNotFoundException("The file was not found");
+            }
         }
 
         public void ChooseWord() {
@@ -27,16 +48,16 @@ namespace HangmanApplication {
             WordDisplay = Enumerable.Repeat('*', WordToGuess.Length).ToArray();
         }
 
-        public static void Proceed(string answer) {
+        public void Proceed(string answer) {
             if (answer.Equals("y")) {
                 Console.WriteLine("Great, let's get started. \n");
                 System.Threading.Thread.Sleep(500);
             } else if (answer.Equals("n")) {
                 Console.WriteLine("Fine, cya later.");
-                System.Threading.Thread.Sleep(1000);
+                System.Threading.Thread.Sleep(500);
                 Environment.Exit(0);
             } else {
-                Console.WriteLine("Cmon buddy, it's a yes or no question. You want to play? (Y/N)");
+                Console.WriteLine("Cmon buddy, it's a yes or no question. Do you want to play? (y/n)");
                 string response = Console.ReadLine().ToLower();
                 Proceed(response);
             }
@@ -44,7 +65,7 @@ namespace HangmanApplication {
 
         public char PromptGuess() {
             Console.WriteLine("Guess a letter: ");
-            char input = System.Convert.ToChar(Console.ReadLine().ToLower().Substring(0, 1));
+            char input = Console.ReadKey().KeyChar.ToString().ToCharArray()[0];
             return input;
         }
 
@@ -58,13 +79,16 @@ namespace HangmanApplication {
 
         public bool CheckGuess(char letter) {
             bool correctGuess = false;
+            // Iterate over the WordToGuess char[] to determine good/bad guess
             for (int i = 0; i < WordToGuess.Count(); i++) {
+                // If the guess was a good guess, file correctGuess to true
                 if (WordToGuess[i].Equals(letter)) {
                     WordDisplay[i] = letter;
                     correctGuess = true;
                 } 
             }
 
+            // If the guess was incorrect, add the letter to IncorrectGuesses
             if (!correctGuess)
                 IncorrectGuesses.Add(letter);
 
@@ -99,8 +123,11 @@ namespace HangmanApplication {
                 Console.WriteLine("\nGuessesRemaining: " + GuessesRemaining);
                 Console.WriteLine("Incorrect Guesses: " + ShowIncorrectGuesses());
                 Console.WriteLine("Here is your word: " + ShowWord() + '\n');
+
+                // Prompt the user for a guess
                 char guess = PromptGuess();
 
+                // Process the guess
                 if (CheckGuess(guess)) {
                     continue;
                 } else {
@@ -108,11 +135,14 @@ namespace HangmanApplication {
                 }
             }
 
+            // If they are out of guesses, tell them they lost
             if (GuessesRemaining == 0) {
                 Console.WriteLine(DisplayMan());
                 Console.WriteLine("Sorry, you're out of guesses. Better luck next time, n00b. \nThe correct answer was " + String.Join("", WordToGuess) + "\n");
                 System.Threading.Thread.Sleep(500);
             }
+
+            // If they have guessed all the letters correctly, tell them that they won
             if (!WordDisplay.Contains('*')) {
                 Console.WriteLine("Congrats buddy, you won. The word was '" + ShowWord() + ".'\n");
                 System.Threading.Thread.Sleep(500);
@@ -122,24 +152,22 @@ namespace HangmanApplication {
         }
 
         static void Main(string[] args) {
-            Hangman hangman = new Hangman();
+            Hangman hangman = new Hangman("../../../HangManWords.txt");
 
-            Console.WriteLine("Ready for a new game of Hangman? (Y/N)");
+            Console.WriteLine("Ready for a new game of Hangman? (y/n)");
             string answer = Console.ReadLine().ToLower();
 
-            while (answer != "n") {
-                Proceed(answer);
+            while (!(String.IsNullOrWhiteSpace(answer))) {
+                hangman.Proceed(answer);
 
                 hangman.ChooseWord();
 
                 hangman.PlayGame();
 
-                Console.WriteLine("Ready for a new game of Hangman? (Y/N)");
+                // After a game is completed, ask the user if they would like to play again
+                Console.WriteLine("Ready for a new game of Hangman? (y/n)");
                 answer = Console.ReadLine().ToLower();
             }
-
-            Console.WriteLine("Thanks for playing");
         }
-
     }
 }
