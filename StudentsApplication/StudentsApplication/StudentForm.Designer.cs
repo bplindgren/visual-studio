0d31298cd64f20b6d9df992843c1ea2fcfd9e963
bplindgren <bplindgren91@gmail.com>
2017-04-08 21:32:36 -0500

    Students progress

---
+++ StudentsApplication/StudentsApplication/StudentForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace StudentsApplication {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.InternetAccess = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            this.NumFailures = new System.Windows.Forms.Button();
+            this.StudyTime = new System.Windows.Forms.Button();
+            this.Absences = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InternetAccess
+            // 
+            this.InternetAccess.Location = new System.Drawing.Point(12, 12);
+            this.InternetAccess.Name = "InternetAccess";
+            this.InternetAccess.Size = new System.Drawing.Size(97, 45);
+            this.InternetAccess.TabIndex = 0;
+            this.InternetAccess.Text = "Internet Access";
+            this.InternetAccess.UseVisualStyleBackColor = true;
+            this.InternetAccess.Click += new System.EventHandler(this.InternetAccess_Click);
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(246, 12);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(326, 20);
+            this.result.TabIndex = 1;
+            this.result.Text = "0";
+            this.result.TextChanged += new System.EventHandler(this.result_TextChanged);
+            // 
+            // NumFailures
+            // 
+            this.NumFailures.Location = new System.Drawing.Point(12, 74);
+            this.NumFailures.Name = "NumFailures";
+            this.NumFailures.Size = new System.Drawing.Size(97, 45);
+            this.NumFailures.TabIndex = 2;
+            this.NumFailures.Text = "Number of Past Failures";
+            this.NumFailures.UseVisualStyleBackColor = true;
+            this.NumFailures.Click += new System.EventHandler(this.NumFailures_Click);
+            // 
+            // StudyTime
+            // 
+            this.StudyTime.Location = new System.Drawing.Point(12, 138);
+            this.StudyTime.Name = "StudyTime";
+            this.StudyTime.Size = new System.Drawing.Size(97, 45);
+            this.StudyTime.TabIndex = 3;
+            this.StudyTime.Text = "Study Time";
+            this.StudyTime.UseVisualStyleBackColor = true;
+            this.StudyTime.Click += new System.EventHandler(this.StudyTime_Click);
+            // 
+            // Absences
+            // 
+            this.Absences.Location = new System.Drawing.Point(12, 204);
+            this.Absences.Name = "Absences";
+            this.Absences.Size = new System.Drawing.Size(97, 45);
+            this.Absences.TabIndex = 4;
+            this.Absences.Text = "Absences";
+            this.Absences.UseVisualStyleBackColor = true;
+            this.Absences.Click += new System.EventHandler(this.Absences_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.Absences);
+            this.Controls.Add(this.StudyTime);
+            this.Controls.Add(this.NumFailures);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.InternetAccess);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Student Grades";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button InternetAccess;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button NumFailures;
+        private System.Windows.Forms.Button StudyTime;
+        private System.Windows.Forms.Button Absences;
+    }
+}
+
---
+++ StudentsApplication/StudentsApplication/StudentForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+
+namespace StudentsApplication {
+    public partial class Form1 : Form {
+
+        DataTable StudentData = new DataTable();
+
+        public Form1() {
+            InitializeComponent();
+            StudentData = GetStudentsDataFromCSVFile(@"C:\Users\Eileen\Documents\Visual Studio 2015\Projects\StudentsApplication\student-mat2.csv");
+        }
+
+        private DataTable GetStudentsDataFromCSVFile(string path) {
+            DataTable csvData = new DataTable();
+            try {
+                using (TextFieldParser csvReader = new TextFieldParser(path)) {
+                    csvReader.SetDelimiters(new string[] { "\";\"", "\";", ";\"", ";" });
+                    csvReader.HasFieldsEnclosedInQuotes = false;
+                    string[] colFields = csvReader.ReadFields();
+                    foreach (string column in colFields) {
+                        DataColumn datecolumn = new DataColumn(column);
+                        datecolumn.AllowDBNull = false;
+                        csvData.Columns.Add(datecolumn);
+                    }
+                    while (!csvReader.EndOfData) {
+                        string[] fieldData = csvReader.ReadFields();
+                        for (int i = 0; i < fieldData.Length; i++) {
+                            string newString = fieldData[i].Replace("\"", "");
+                            fieldData[i] = newString;
+                        }
+                        csvData.Rows.Add(fieldData);
+                    }
+                }
+            } catch (Exception ex) {
+                Console.Write(ex.Message);
+            }
+            foreach (DataRow row in csvData.Rows) {
+                Console.WriteLine();
+                for (int x = 0; x < csvData.Columns.Count; x++) {
+                    Console.Write(row[x].ToString() + " ");
+                }
+            }
+            return csvData;
+        }
+
+        public float CalculateAverageGrade(EnumerableRowCollection students) {
+            int total = 0;
+            int numStudents = 0;
+            foreach (DataRow row in students) {
+                int j;
+                Int32.TryParse(row["G3"].ToString(), out j);
+                total += j;
+                numStudents++;
+            }
+            return ((float)total/(float)numStudents);
+        }
+
+        private void InternetAccess_Click(object sender, EventArgs e) {
+            // Get the rows where students had the internet
+            EnumerableRowCollection yesRows = StudentData.AsEnumerable()
+                .Where(row => row.Field<string>("internet").Contains("yes"));
+
+            // Get the rows where students did not have the internet
+            EnumerableRowCollection noRows = StudentData.AsEnumerable()
+                .Where(row => row.Field<string>("internet").Contains("no"));
+
+            result.Text = "The average grade of students with internet access was " + CalculateAverageGrade(yesRows) + ".\n" + "The average grade of students without internet access was " + CalculateAverageGrade(noRows) + ".";
+        }
+
+        private void NumFailures_Click(object sender, EventArgs e) {
+            result.Text = StudentData.Rows[3]["failures"].ToString();
+        }
+
+        private void StudyTime_Click(object sender, EventArgs e) {
+            result.Text = StudentData.Rows[3]["studytime"].ToString();
+        }
+
+        private void Absences_Click(object sender, EventArgs e) {
+            result.Text = StudentData.Rows[3]["absences"].ToString();
+        }
+
+        private void result_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
